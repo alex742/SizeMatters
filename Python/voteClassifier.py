e763a8d3a098a6f999c6d215b5b4af5627b59bdf
Alex White <alex_white742@outlook.com>
2020-02-20 16:17:54 +0000

    Passing in data to vote - need to convert all features to strings

--- Python/voteClassifier.py
+++ Python/voteClassifier.py
@@ -1,6 +1,5 @@
 import nltk
 import random
-from nltk.corpus import movie_reviews
 from nltk.classify.scikitlearn import SklearnClassifier
 import pickle
 
@@ -41,33 +40,16 @@ class VoteClassifier(ClassifierI):
         conf = choice_votes / len(votes)
         return conf
 
-documents = [(list(movie_reviews.words(fileid)), category)
-             for category in movie_reviews.categories()
-             for fileid in movie_reviews.fileids(category)]
+inputFile = open("labelledFeatures.pickle","rb")
+featuresets = pickle.load(inputFile)
+inputFile.close()
 
-random.shuffle(documents)
-
-all_words = []
-
-for w in movie_reviews.words():
-    all_words.append(w.lower())
-
-all_words = nltk.FreqDist(all_words)
-
-word_features = list(all_words.keys())[:3000]
-
-def find_features(document):
-    words = set(document)
-    features = {}
-    for w in word_features:
-        features[w] = (w in words)
-
-    return features
-
-featuresets = [(find_features(rev), category) for (rev, category) in documents]
+print(len(featuresets))
         
-training_set = featuresets[:1900]
-testing_set =  featuresets[1900:]
+training_set = featuresets[:18]
+testing_set =  featuresets[18:]
+
+print(testing_set)
 
 ####################################################
 #  [ (features , class) ]
